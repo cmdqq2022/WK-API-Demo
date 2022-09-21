970385b7feab26b7bf8408a4a913857a055c79fe
cmdqq2022 <113968548+cmdqq2022@users.noreply.github.com>
2022-09-21 11:04:33 +0800

    Create gradient-bg.vue

---
+++ src/components/tv-demos/gradient-bg.vue
@@ -0,0 +1,46 @@
+<template>
+
+
+  <div class="divg">
+    <div  :enableFocusBorder="true" :focusable="true" style="width: 300px;height: 200px; background-color: transparent "
+          :gradientBackground="gradientBackground">
+    </div>
+    <!-----cornerRadius:10 --->
+    <!-----top-left, top-right, bottom-right, bottom-left --->
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+import { getApp } from '@/util';
+import native from '@/native';
+export default {
+  data() {
+    return {
+      gradientBackground: {
+        type: 0,
+        shape: 0,
+        colors: ['#40b883', '#4068b8'],
+        cornerRadii4: [0, 5, 20, 0],
+      },
+    };
+  },
+  methods: {
+    backPress() {
+      native.closePage()
+    },
+  },
+  mounted() {
+    this.app = getApp();
+    this.app.$on('hardwareBackPress', this.backPress);
+  },
+};
+</script>
+<style>
+  .divg{
+    width: 1920px;
+    height: 1080px;
+    padding: 50px;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
